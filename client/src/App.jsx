9f71b9bb1aae215eb969868aa1db6008670a94b6
Ali Mammadli <138757971+elimemmedli8@users.noreply.github.com>
2023-12-12 20:37:30 +0400

    2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Todo from "./components/Todo";
 
 function App() {
   const [login, setLogin] = useState(false);
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState(undefined);
   return (
     <div>
       {login ? (
